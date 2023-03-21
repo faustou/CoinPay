aa848c6d0c1477efbb44d875d5b292233085cc06
Fausto Scarmato <110869020+faustou@users.noreply.github.com>
2023-03-20 23:52:49 -0300

    first commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './NavBar.scss'
+
+const NavBar = () => {
+    return (
+        <div className='nav'>
+            <div>
+                <h2 className='logo-coinpay'>CoinPay</h2>
+            </div>
+            <div>
+                <li>Home</li>
+                <li>Market</li>
+                <li>Tarjeta</li>
+                <li>FAQ</li>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
